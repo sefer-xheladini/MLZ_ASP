6b21c881aa91018e8fd33303c98c3e956085cbbc
unknown <sefer.xheladini@gmail.com>
2019-01-07 18:57:45 +0100

    Building-Model

--- MLZ_Sefer-Xheladini/Models/Building.cs
+++ MLZ_Sefer-Xheladini/Models/Building.cs
@@ -7,5 +7,17 @@ namespace MLZ_Sefer-Xheladini.Models
 {
     public class Building
 {
+    public BuildingType buildingType { get; set; }
+    public int Beds { get; set; }
+    public string Condition { get; set; }
+    public double Price { get; set; }
+    public bool Wlan { get; set; }
+    public bool WlanFree { get; set; }
+    public double WlanPrice { get; set; }
+    public bool Parking { get; set; }
+    public bool ParkingFree { get; set; }
+    public double ParkingPrice { get; set; }
+    public double Space { get; set; }
+    public bool Balkony { get; set; }
 }
 }
