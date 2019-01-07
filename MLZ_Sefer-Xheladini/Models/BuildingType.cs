aaa299edea66e2b3fd3dfcc6224a558554ddfeb1
unknown <sefer.xheladini@gmail.com>
2019-01-07 18:38:10 +0100

    Build Repo

---
+++ MLZ_Sefer-Xheladini/Models/BuildingType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MLZ_Sefer-Xheladini.Models
+{
+    public class BuildingType
+{
+}
+}
