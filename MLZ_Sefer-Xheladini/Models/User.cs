4801d03847a9841f784677e0227b51f1159f9188
unknown <sefer.xheladini@gmail.com>
2019-01-10 00:24:47 +0100

    added usertypes

--- MLZ_Sefer-Xheladini/Models/User.cs
+++ MLZ_Sefer-Xheladini/Models/User.cs
@@ -12,6 +12,6 @@ namespace MLZ_Sefer_Xheladini.Models
     public string Firstname { get; set; }
     public string Lastname { get; set; }
     public string Password { get; set; }
-    public UserType UserType { get; set; }
+        public UserType UserType { get; set; }
 }
 }
---
+++ MLZ_Sefer-Xheladini/Query/Insert_UserType - Kopieren.sql
@@ -0,0 +1,4 @@
+﻿USE [MLZ_Sefer_XheladiniContext-91a554d6-4ae9-421a-a0ce-46edd2835ce0]
+
+INSERT INTO [dbo].[User] (Firstname, Lastname, email, password)
+values ('firstname', 'lastname', 'test@em.ch', '1234')
